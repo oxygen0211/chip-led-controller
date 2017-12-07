834d5e1424e47d702fe1eb9ebdadd780192b0150
Julian Engelhardt <jengelhardt211@gmail.com>
2017-12-07 22:20:25 +0100

    Replace setting binary values for LEDs with SPWM for supporting RGB vals
    For enabling more colors by mixing RGB values. Playing around with the
    frequency might be a good idea. Or my LEDs suck even more than I
    initially thought :-(

--- gpiocontrol.py
+++ gpiocontrol.py
@@ -1,21 +1,34 @@
 import CHIP_IO.GPIO as GPIO
+import CHIP_IO.SOFTPWM as SPWM
 
 class LED_GPIO:
     def __init__(self):
         green = 'XIO-P0'
         blue = 'XIO-P1'
         red = 'XIO-P2'
+        frequency = 2500
 
         self.green = green
         self.red = red
         self.blue = blue
 
-        GPIO.setup(green, GPIO.OUT)
-        GPIO.setup(blue, GPIO.OUT)
-        GPIO.setup(red, GPIO.OUT)
+        GPIO.cleanup(red)
+        GPIO.cleanup(green)
+        GPIO.cleanup(blue)
+
+        SPWM.start(red, 0)
+        SPWM.set_frequency(red, frequency)
+        SPWM.start(green, 0)
+        SPWM.set_frequency(green, frequency)
+        SPWM.start(blue, 0)
+        SPWM.set_frequency(blue, frequency)
 
     def setPin(self, pin, value):
-        GPIO.output(pin, value)
+        # we assume RGB values from 0 to 255, but SPWM expects 0 to 100,
+        # so we have to normalize
+        duty = value/2.55
+        print('setting value '+str(duty)+' for '+pin)
+        SPWM.set_duty_cycle(pin, duty)
 
     def setRed(self, value):
         self.setPin(self.red, value)
